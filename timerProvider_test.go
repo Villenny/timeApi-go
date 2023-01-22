da78749e9800698f22a97ef1dbb94b90cb04e854
Ryan Haksi <ryan.haksi@gmail.com>
2023-01-21 19:15:15 -0800

    - change code style to more community friendly

--- timerProvider_test.go
+++ timerProvider_test.go
@@ -10,9 +10,9 @@ import (
 func TestStartStopTimer(t *testing.T) {
 
 	now := time.Date(2009, 11, 17, 20, 34, 58, 0, time.UTC)
-	timeApi_ := NewFake().Start(now)
+	timeapi := NewFake().Start(now)
 
-	timerProvider, _ := NewTimerProvider(timeApi_)
+	timerProvider, _ := NewTimerProvider(timeapi)
 	var runCount int
 	const CHECK_INTERVAL = 100 * time.Millisecond
 	timer := timerProvider.SetInterval(func() { runCount += 1 }, CHECK_INTERVAL)
@@ -20,15 +20,15 @@ func TestStartStopTimer(t *testing.T) {
 	assert.Equal(t, timer.Count(), runCount)
 
 	timer.WaitUntilCount(0)
-	timeApi_.IncrementClock(CHECK_INTERVAL)
+	timeapi.IncrementClock(CHECK_INTERVAL)
 	timer.WaitUntilCount(0)
 	timer.WaitUntilCount(1)
 	timer.ClearInterval()
 	assert.Equal(t, 1, runCount)
 	assert.Equal(t, timer.Count(), runCount)
 
-	timeApi_.IncrementClock(CHECK_INTERVAL)
-	timeApi_.Stop()
+	timeapi.IncrementClock(CHECK_INTERVAL)
+	timeapi.Stop()
 	assert.Equal(t, 1, runCount)
 	assert.Equal(t, timer.Count(), runCount)
 }
